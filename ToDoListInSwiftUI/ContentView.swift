e8ef5252a8940e57d10719dfc1324e68d121a061
hidec-7 <hideto.dev7@gmail.com>
2024-11-09 21:28:47 +0900

    project up

---
+++ ToDoListInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListInSwiftUI
+//
+//  Created by h-chaya on 2024/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListInSwiftUI/ToDoListInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListInSwiftUIApp.swift
+//  ToDoListInSwiftUI
+//
+//  Created by h-chaya on 2024/11/09.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
